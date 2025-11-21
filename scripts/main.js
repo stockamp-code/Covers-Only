1aa2608b2d3266687d1f0f4310478bc09e683554
Joel Stockamp <joelstockamp@gmail.com>
2025-11-21 10:13:06 -0800

    added many fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 let sheet_csv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRriKSnmOyDMzu0qAznH52vJfkSDH1kQuIilXU8bm2oFLYPbDusjHajHXPAjBh7-ff4x6EopMC1MXlM/pub?gid=552228275&single=true&output=csv';
-let siteData = {};
-
+let siteData = {
+  'signupRequests' : {},
+  'orderList' : [],
+  'previousPerformersList' : []
+};
 
 
 function fetch_data(sheet_csv) {
@@ -9,38 +12,75 @@ function fetch_data(sheet_csv) {
   .then(function(data){parseData(data)});
 
   const parseData = function(data){
-  let gson = Papa.parse(data, {header:true}).data;
-  renderData(gson);
+    let gson = Papa.parse(data, {header:true}).data;
+    renderData(gson);
   };
 
   const renderData = function(gson) {
-    let signupRequests = siteData;
+    let signupRequests = siteData.signupRequests;
+    
     for (let i=0; i<gson.length; i++) {
       const entry = gson[i];
+      // console.log(entry)
+
+      const timestamp = entry.Timestamp;
+      const name = entry.Name;
+      const systemName = name.toLowerCase();
+      const setup = entry.Setup.split(", "); 
+      let instagram = entry.Instagram;
+      let requestEntries = {};
+      let duplicateStatus = false;
       let requestNumber = 1;
-      if (siteData[entry.Name]){
-       console.log("duplicate")
-       console.log(entry.Name)
-       requestNumber = siteData[entry.Name].requestNumber + 1;
-       console.log(requestNumber);
+
+      if (signupRequests[systemName]){
+        if (instagram === ""){
+          instagram = signupRequests[systemName].instagram;
+        };
+        requestEntries = signupRequests[systemName].requestEntries
+
+        requestNumber = Object.keys(requestEntries).length + 1;
+
+        if (requestEntries[timestamp]){
+          duplicateStatus = true;
+        }
       }
 
-      let entryObject = {
-        'name' : entry.Name+" ["+requestNumber+"]",
-        'time' : entry.Timestamp,
-        'setup' : entry.Setup,
-        'instagram' : entry.Instagram,
-        'requestNumber' : requestNumber
+      if (duplicateStatus){
+        console.log("Duplicate Status: " + duplicateStatus)
+      } else {
+        let requestEntry = {
+          'name' : name,
+          'setup' : setup,
+          'instagram' : instagram,
+          'timestamp' : timestamp,
+          'requestNumber' : requestNumber,
+          'alreadyPerformed' : false
+        }
+
+        requestEntries[timestamp] = requestEntry;
+
+        let personData = {
+          'name' : name,
+          'instagram' : instagram,
+          'requestEntries' : requestEntries,
+          'totalRequestCount' : requestNumber,
+        }
+        
+        signupRequests[systemName] = personData;
       }
-      
-      signupRequests[entry.Name] = entryObject
-      // console.log(entryObject)
     }
-    console.log(signupRequests)
+
+    siteData.signupRequests = signupRequests;
+    console.log(siteData)
   }
 
 }
 
+function sortOrderList(signupRequests, orderList, previousPerformersList) {
+  console.log("Howdy")
+}
+
 fetch_data(sheet_csv)
+sortOrderList(siteData.signupRequests, siteData.orderList, siteData.previousPerformersList)
 
 
