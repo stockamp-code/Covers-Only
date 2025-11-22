12bd8ee850442749ead7fa6544aef219b1d28833
Joel Stockamp <joelstockamp@gmail.com>
2025-11-22 02:14:13 -0800

    added buttons and render data for the various lists

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 let signup_csv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRriKSnmOyDMzu0qAznH52vJfkSDH1kQuIilXU8bm2oFLYPbDusjHajHXPAjBh7-ff4x6EopMC1MXlM/pub?gid=552228275&single=true&output=csv';
 let siteData = {}
+const signUpDivID = "signup_list";
+const previousPerformersDivID = "previous_performers"
+
+// ===============================================================================
 
 function writeToLocalStorage(data){
   const newData = JSON.stringify(data);
@@ -20,7 +24,10 @@ function initializeLocalStorage(){
   writeToLocalStorage(blankData)
   pullFromLocalStorage();
   console.log("initialized local storage")
+  renderSiteData();
 }
+
+// =========================================================================
 
 function fetch_data(signup_csv) {
   fetch(signup_csv)
@@ -57,19 +64,21 @@ function fetch_data(signup_csv) {
           instagram = signupRequests[systemName].instagram;
         };
         requestEntries = signupRequests[systemName].requestEntries;
-
-        requestNumber = Object.keys(requestEntries).length + 1;
-
-        if (requestEntries[timestamp]){
+        requestNumber = Object.keys(requestEntries).length+1;
+        
+        if (requestEntries[numericalTimestamp]){
           duplicateStatus = true;
+        } else {
         };
       };
 
       if (duplicateStatus){
         console.log("Duplicate Status: " + duplicateStatus)
       } else {
+
         let requestEntry = {
           'name' : name,
+          'systemName' : systemName,
           'setup' : setup,
           'instagram' : instagram,
           'timestamp' : timestamp,
@@ -78,7 +87,7 @@ function fetch_data(signup_csv) {
           'alreadyPerformed' : false
         };
 
-        requestEntries[timestamp] = requestEntry;
+        requestEntries[numericalTimestamp] = requestEntry;
 
         let personData = {
           'name' : name,
@@ -95,6 +104,7 @@ function fetch_data(signup_csv) {
     sortOrderList()
     writeToLocalStorage(siteData);
     pullFromLocalStorage();
+    renderSiteData();
     console.log(siteData);
   };
 };
@@ -121,13 +131,100 @@ function sortOrderList() {
   );
   newPreviousPerformersList.sort(
     (a, b) => 
-      a.requestNumber - b.requestNumber || a.numericalTimestamp - b.numericalTimestamp
+      b.requestNumber - a.requestNumber || b.numericalTimestamp - a.numericalTimestamp
   );
-  // console.log(newList)
-  // console.log(newPreviousPerformersList)
   siteData.orderList = newList;
   siteData.previousPerformersList = newPreviousPerformersList;
 };
+
+function toggleAlreadyPerformedStatus(personID, requestID){
+  if(siteData.signupRequests[personID]){
+    let thisRequest = siteData.signupRequests[personID].requestEntries[requestID].alreadyPerformed;
+    siteData.signupRequests[personID].requestEntries[requestID].alreadyPerformed = !thisRequest;   
+  }
+  sortOrderList()
+  writeToLocalStorage(siteData)
+  pullFromLocalStorage()
+  console.log(siteData.signupRequests[personID].requestEntries[requestID])
+}
+
+// ========================================================================================
+
+function renderSiteData(){
+  const orderList = siteData.orderList;
+  const previousPerformersList = siteData.previousPerformersList;
+  const signUpDiv = document.getElementById(signUpDivID);
+  const prevDiv = document.getElementById(previousPerformersDivID);
+
+  signUpDiv.innerHTML = "";
+  prevDiv.innerHTML = "";
+
+  orderList.forEach(request =>{
+    const name = request.name;
+    const systemName = name.toLowerCase()
+    const id = request.numericalTimestamp;
+    const requestNumber = request.requestNumber;
+    const instagram = request.instagram;
+
+    let newRequestDiv = document.createElement("div");
+    
+    newRequestDiv.setAttribute("id", id);
+    newRequestDiv.classList.add("request-wrapper");
+    newRequestDiv.innerHTML = `
+    <div class="artist-name">
+    ${name} <span class="request-number">${requestNumber}</span>
+    </div>
+    <div class="instagram">
+    ${instagram}
+    </div>
+    `
+    let button = document.createElement("button");
+    button.classList.add("toggle-button");
+    button.innerHTML = "Switch";
+    button.onclick = function() {
+      console.log(systemName)
+      console.log(id)
+      toggleAlreadyPerformedStatus(systemName, id);
+      renderSiteData()
+    }
+    newRequestDiv.appendChild(button);
+    signUpDiv.appendChild(newRequestDiv)
+  })
+
+    previousPerformersList.forEach(request =>{
+    const name = request.name;
+    const systemName = name.toLowerCase();
+    const id = request.numericalTimestamp;
+    const requestNumber = request.requestNumber;
+    const instagram = request.instagram;
+
+    let newRequestDiv = document.createElement("div");
+    
+    newRequestDiv.setAttribute("id", id);
+    newRequestDiv.classList.add("request-wrapper");
+    newRequestDiv.innerHTML = `
+    <div class="artist-name">
+    ${name} <span class="request-number">${requestNumber}</span>
+    </div>
+    <div class="instagram">
+    ${instagram}
+    </div>
+    `
+    let button = document.createElement("button");
+    button.classList.add("toggle-button");
+    button.innerHTML = "Switch";
+    button.onclick = function() {
+      console.log(systemName)
+      console.log(id)
+      toggleAlreadyPerformedStatus(systemName, id);
+      renderSiteData()
+    }
+    newRequestDiv.appendChild(button);
+    prevDiv.appendChild(newRequestDiv)
+  })
+}
+
+// ==================================================================================
 
 if (!localStorage.siteData) {
   initializeLocalStorage();
